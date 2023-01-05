55f6c96f486b8a96b48365b589a88a147f6a610e
kvvivvic <rnjs9337@gmail.com>
2023-01-05 17:57:51 +0900

    useEffect 맛보기

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -19,5 +19,4 @@ function AppProfile() {
     </>
   );
 }
-
 export default AppProfile;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import "./index.css";
 
 import reportWebVitals from "./reportWebVitals";
 
-import AppCounter from "./AppCounter";
+import AppProducts from "./AppProducts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppCounter />
+    <AppProducts />
   </React.StrictMode>
 );
 
